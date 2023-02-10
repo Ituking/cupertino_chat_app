397b8a87dae982825279e8b24c756a8fcdd6eaef
Ituking <77528963+Ituking@users.noreply.github.com>
2023-02-10 12:28:06 +0900

    タブバーのリサイズ設定

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       child: CupertinoTabScaffold(
+        resizeToAvoidBottomInset: true,
         tabBar: CupertinoTabBar(
           items: const [
             BottomNavigationBarItem(
