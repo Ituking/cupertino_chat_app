2df453e64d8c3c94ebfba287487adf842e5e95dc
Ituking <77528963+Ituking@users.noreply.github.com>
2023-02-13 12:52:19 +0900

    Firebase連携のテスト

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,39 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cupertino_chat_app/screens/calls.dart';
 import 'package:cupertino_chat_app/screens/chats.dart';
 import 'package:cupertino_chat_app/screens/people.dart';
 import 'package:cupertino_chat_app/screens/settings.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 
-void main() {
+const bool useEmulator = true;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+  await Firebase.initializeApp();
+  if (useEmulator) {
+    _connectToFirebaseEmulator();
+  }
   runApp(const MyApp());
+}
+
+Future _connectToFirebaseEmulator() async {
+  const fireStorePort = "8080";
+  const authPort = "9099";
+  final localHost = Platform.isAndroid ? "10.0.2.2" : "localhost";
+  FirebaseFirestore.instance.settings = Settings(
+    host: "$localHost:$fireStorePort",
+    sslEnabled: false,
+    persistenceEnabled: false,
+  );
+
+  await FirebaseAuth.instance.useAuthEmulator(
+    "http://$localHost",
+    "http://$authPort" as int,
+  );
 }
 
 class MyApp extends StatelessWidget {
