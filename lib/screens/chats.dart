d538fe4ec3961498065f9b28e9c499ed94002fd8
Ituking <77528963+Ituking@users.noreply.github.com>
2023-02-14 18:55:03 +0900

    Firebase連携のテスト

--- lib/screens/chats.dart
+++ lib/screens/chats.dart
@@ -1,12 +1,62 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 
 class Chats extends StatelessWidget {
   const Chats({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Chats"),
-    );
+    return StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("chats").snapshots(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Something went wrong"),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: Text("Loading"),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return const CustomScrollView(
+              slivers: [
+                CupertinoSliverNavigationBar(
+                  largeTitle: Text("Chats"),
+                )
+              ],
+            );
+          }
+          return Container();
+        });
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return CustomScrollView(
+  //     slivers: [
+  //       const CupertinoSliverNavigationBar(
+  //         largeTitle: Text("Chats"),
+  //       ),
+  //       SliverList(
+  //         delegate: (SliverChildListDelegate(
+  //           [
+  //             const CupertinoListTile(
+  //               title: Text("Chat1"),
+  //             ),
+  //             const CupertinoListTile(
+  //               title: Text("Chat2"),
+  //             ),
+  //             const CupertinoListTile(
+  //               title: Text("Chat3"),
+  //             ),
+  //           ],
+  //         )),
+  //       ),
+  //     ],
+  //   );
+  // }
 }
