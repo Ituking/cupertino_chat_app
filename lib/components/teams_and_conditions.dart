cf11ecb213e4ffd7d9a0f4bfacb1e2990faa62b5
Ituking <77528963+Ituking@users.noreply.github.com>
2023-02-15 21:24:43 +0900

    teams_and_conditions.dartの作成

---
+++ lib/components/teams_and_conditions.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TeamsAndConditions extends StatelessWidget {
+  const TeamsAndConditions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
