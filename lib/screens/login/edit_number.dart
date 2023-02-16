b651399406ad59f0294e8f53982c7dd1508ab07a
Ituking <77528963+Ituking@users.noreply.github.com>
2023-02-16 16:23:06 +0900

    Flutterのエラー対応（完了）
    The following assertion was thrown during layout:
    A RenderFlex overflowed by 10.0 pixels on the right.

--- lib/screens/login/edit_number.dart
+++ lib/screens/login/edit_number.dart
@@ -23,13 +23,15 @@ class _EditNumberState extends State<EditNumber> {
           Row(
             children: [
               const Logo(width: 80.0, height: 80.0, radius: 30.0),
-              Text(
-                "Verification ・ one step",
-                style: TextStyle(
-                  color: const Color(0xFF08C187).withOpacity(0.7),
-                  fontSize: 30,
+              Expanded(
+                child: Text(
+                  "Verification ・ one step",
+                  style: TextStyle(
+                    color: const Color(0xFF08C187).withOpacity(0.7),
+                    fontSize: 30,
+                  ),
                 ),
-              ),
+              )
             ],
           ),
         ],
